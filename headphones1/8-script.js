6a7065408f46b4c2c176d334ca6610cbcec54d54
Atokolo John <156380377+Redfox1759@users.noreply.github.com>
2025-06-21 00:01:47 +0100

    Delete headphones1 directory

--- headphones1/8-script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.getElementsByClassName('menu-icon');
-    const navbarMenu = document.getElementsByClassName('navbar-menu');
-
-    menuIcon.addEventListener('click', function () {
-      navbarMenu.classList.toggle('show');
-      menuIcon.classList.toggle('open');
-    });
-});
